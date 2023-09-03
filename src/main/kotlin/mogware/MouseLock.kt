0b6bc21e14ffe19db1a8234ea6a1d82335a8c7d4
mog <82464360+eastoid@users.noreply.github.com>
2023-09-03 16:46:12 +0200

    totalus refactor
    now prevents sleep mode by default  -
    or just moves the mouse idk

--- src/main/kotlin/mogware/MouseLock.kt
+++ src/main/kotlin/mogware/MouseLock.kt
@@ -3,9 +3,7 @@ package mogware
 import com.github.kwhat.jnativehook.GlobalScreen
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.awt.Desktop
 import java.awt.MouseInfo
 import java.awt.Point
@@ -17,98 +15,109 @@ import java.util.*
 import kotlin.system.exitProcess
 
 
-
-
-/*
-
-    Gonna refactor code eventually
-    for now enjoy true italian spaghetti
-
-
-
-
-*/
-
-
-
-
-
-
+// Turn on / off printing of program output
+const val printOutput: Boolean = true
+// Turn on / off printing of logging prompt
+const val printLoggingPrompt: Boolean = true
+// Turn on / off printing of error messages on program exit
+const val printErrorMessage: Boolean = true
 
 // Important variable - First time setup prompt  (Enable or disable logging prompt) - true by default, false allows running program first time without UI     -     Default logging is turned ON without prompt!
-val allowPrompt = false
+const val allowPrompt: Boolean = false
+
+// Allow program to prevent sleep mode
+const val allowPreventSleep: Boolean = true
+const val preventSleepTimer: Long = 30000L
 
 // Enables logging into a file: (yes / no) - Default: YES (Change in config file)
-var enableLogging = "yes"
+var enableLogging: String /* yes / no */ = "yes"
 
-// Change mouse position X Y here: >>
-val positionHorizontal = 400
-val positionVertical = 400
-val checkForMouseMovementDelay = 10L    // Delay for checking for mouse movement (optimization, limiting resource usage) in Milliseconds (Must be a Long value - include L behind value)<<<
-//                                 <<
+//  Change mouse position X Y here (First number)                                                                                                                                     !!! Important value
+const val positionHorizontal: Int = 400
+const val positionVertical: Int = 400
+const val checkForMouseMovementDelay: Long = 10L    // Delay for checking for mouse movement (optimization, limiting resource usage) in Milliseconds (Must be a Long value - include L behind value)<<<
 
-var file = File("empty") // Logging enabled by default
-val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+// Exception
+var exception: String = ""
 
 
+var file: File = File("empty") // Logging enabled by default
+val formatter: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
 
 
 fun main() {
 
     val createFile = File("${System.getProperty("user.home")}\\MouseMovementLog.txt")
-    val checkConfig = checkConfig(createFile)
+    val checkConfig: String = checkConfig(createFile)
 
+
+    if (allowPreventSleep && printOutput) println("Program is preventing sleep!")
 
     if (checkConfig == "null") {
         if (allowPrompt) {
-            println("Do you want to log activity in user directory file MouseMovementLog.txt? (First time configuration - Configurable in log file) -  yes / no:")
-            if (readLine() != null && readLine()!!.lowercase().contains("yes")) {
+            if (printLoggingPrompt) println("Do you want to log activity in user directory file MouseMovementLog.txt? (First time configuration - Configurable in log file) -  yes / no:")
+            if (readlnOrNull() != null && readln().lowercase().contains("yes")) {
                 enableLogging = "yes"
             }
         }
     }
 
 
-
-    val date: Date = Date()
+    val date = Date()
     val startText = "\n\n\n---- Start of program  |  $date ---- Time: Hour.Minute.Second\n\n"
-    val writer = FileWriter(createFile, true)
 
-    when (checkConfig) {
-        "null" -> {
-            println("Logging into a file is enabled ($createFile)")
-            if (createFile.isFile && !createFile.exists()) { createFile.createNewFile() }
-            writer.write("Configuration (Allow / disallow logging movements into THIS file) -> allowLogging:yes$startText")
-            file = createFile
-            enableLogging = "yes"
+    val writer = FileWriter(createFile, true).use {writer ->
+        when (checkConfig) {
+            "null" -> {
+                if (printOutput) println("Logging into a file is enabled ($createFile)")
+                if (createFile.isFile && !createFile.exists()) {
+                    try {
+                        createFile.createNewFile()
+                    }catch (e: Exception) {
+                         exception = e.stackTraceToString()
+                        if (printOutput) println("There was an error creating file \"$createFile\"! Close the program to print the full message.")
+                    }
+                }
+                writer.write("Configuration (Allow / disallow logging movements into THIS file) -> allowLogging:yes$startText")
+                file = createFile
+                enableLogging = "yes"
 
-        }
-        "no" -> {
-            println("Logging is disabled! Enable in ${System.getProperty("user.home")}\\MouseMovementLog.txt")
-            enableLogging = "no"
+            }
 
-        }
-        "yes" -> {
-            file = createFile
-            writer.write(startText)
-            enableLogging = "yes"
-            println("Logging enabled (Change in log file: $createFile)")
+            "no" -> {
+                if (printOutput) println("Logging is disabled! Enable in ${System.getProperty("user.home")}\\MouseMovementLog.txt")
+                enableLogging = "no"
 
+            }
+
+            "yes" -> {
+                file = createFile
+                writer.write(startText)
+                enableLogging = "yes"
+                if (printOutput) println("Logging enabled (Change in log file: $createFile)")
+
+            }
         }
     }
 
-    writer.close()
 
-    useless(positionHorizontal, positionVertical)
+    runBlocking {
+        mouselockRunner(positionHorizontal, positionVertical)
+    }
 
 }
 
 
+// important variables
 var status: String = "stay"
 val robot = Robot()
 var movements = 0
+//
 
-fun useless(posHor: Int, posVer: Int) {
+
+// function to lock the mouse if movement detected.
+suspend fun mouselockRunner(posHor: Int, posVer: Int) = runBlocking{
+
     //println("Ctrl Q")
     GlobalScreen.registerNativeHook()
 
@@ -116,29 +125,41 @@ fun useless(posHor: Int, posVer: Int) {
 
     robot.mouseMove(positionHorizontal, positionVertical) // Initial movement to prevent false detection
 
-
-    // >> Get screen size (not utilized)
-//    val screenSize: Dimension = Toolkit.getDefaultToolkit().screenSize
-//    val width = screenSize.getWidth()
-//    val height = screenSize.getHeight()
-    // << Can use for different purpose
-
-
-    /** Delayed loop to check for mouse movement */
-    while (status == "stay") {
-        val position: Point = MouseInfo.getPointerInfo().location
-        if (position.x != positionHorizontal || position.y != positionVertical) {
-
-            centerMouseFunction(position.x, position.y)
-        }
-
-        Thread.sleep(checkForMouseMovementDelay)
+    val preventSleepFunction = GlobalScope.launch(Dispatchers.Default) {
+        unsleepDevice()
     }
+
+
+    // loop that checks for mouse position changes (and if prevent sleep is on)
+    if (allowPreventSleep) {
+
+        while (status == "stay") {
+            val position: Point = MouseInfo.getPointerInfo().location
+            if (position.x != positionHorizontal || position.y != positionVertical) {
+                if (position.x != positionHorizontal + 1 || position.y != positionVertical + 1) {
+                    centerMouseFunction(position.x, position.y)
+                }
+
+            }
+
+            Thread.sleep(checkForMouseMovementDelay)
+        }
+    } else {
+        while (status == "stay") {
+            val position: Point = MouseInfo.getPointerInfo().location
+            if (position.x != positionHorizontal || position.y != positionVertical) {
+                centerMouseFunction(position.x, position.y)
+            }
+            Thread.sleep(checkForMouseMovementDelay)
+        }
+    }
+
 
 
 }
 
-/** Function to handle mouse movement */
+
+// function that handles mouse movement
 fun centerMouseFunction(posHor: Int, posVer: Int) {
     val date = "   ${Date()}   "
 
@@ -148,12 +169,18 @@ fun centerMouseFunction(posHor: Int, posVer: Int) {
         val warningOne = "Device is in sleep mode:  $date  -   positions: $posHor, $posVer"
         if (enableLogging == "yes") {
             CoroutineScope(Dispatchers.IO).launch {
-                println("Logging...")
-                val writer = FileWriter(file, true)
-                writer.write("$warningText\n$warningOne\n\n")
-                writer.close()
+
+                try {
+                    FileWriter(file, true).use {writer ->
+                        writer.write("$warningText\n$warningOne\n\n")
+                    }
+                    println("Logging...")
+                } catch (e: Exception) {
+                    val message: String = if (!exception.isBlank()) "This is probably because there was an error creating the logfile." else ""
+                    if (printErrorMessage && printOutput) println("There was en error writing output into the log file. $message")
+                }
             }
-            println("$warningText\n$warningOne")
+            if (printOutput) println("$warningText\n$warningOne")
         }
 
         return
@@ -166,38 +193,70 @@ fun centerMouseFunction(posHor: Int, posVer: Int) {
 
     if (enableLogging == "yes") {
         CoroutineScope(Dispatchers.IO).launch {
-            println("Logging...")
-            val writer = FileWriter(file, true)
-            writer.write("$warning\n\n")
-            writer.close()
+
+            try {
+                FileWriter(file, true).use {writer ->
+                    writer.write("$warning\n\n")
+                }
+                if (printOutput) println("Logging...")
+            } catch (e: Exception) {
+                val message: String = if (!exception.isBlank()) "This is probably because there was an error creating the logfile." else ""
+                if (printErrorMessage && printOutput) println("There was en error writing output into the log file. $message")
+            }
         }
     }
 
-    println(warning)
+    if (printOutput) println(warning)
 
     for (i in 1..10000) {
-        //println("Testing purpose: i: '$i' - After movement loop (10,000 passes)")   << Print every movement pass after mouse detection is triggered - 10,000 passes by default
         robot.mouseMove(positionHorizontal, positionVertical)
     }
-    println("- Movement timeout - ")
 
 
 }
 
 
 
+// function to prevent device from sleeping
+suspend fun unsleepDevice() {
+    if (!allowPreventSleep) return
 
-/** Function for global key listening to exit program (default CTRL+Q) */
+
+    while (allowPreventSleep) {
+        delay(preventSleepTimer)
+        robot.mouseMove(positionHorizontal + 1, positionVertical + 1)
+        robot.mouseMove(positionHorizontal, positionVertical)
+        if (status != "stay") exitProcess(500)
+    }
+
+}
+
+
+
+// set global key listener
 class GlobalKeyListener : NativeKeyListener {
     override fun nativeKeyPressed(event: NativeKeyEvent) {
+
         // Exit if user presses Ctrl + Q
         if (event.keyCode == NativeKeyEvent.VC_Q && event.modifiers and NativeKeyEvent.CTRL_MASK != 0) {
             status = "stop"
+
             GlobalScreen.unregisterNativeHook()
-            val writer = FileWriter(file, true)
-            writer.write("\n\n -- PROGRAM CLOSED -- ${Date()}\n//////\n")
-            writer.close()
-            exitProcess(69)
+
+            if (enableLogging.contains("y")) {
+                try {
+                    FileWriter(file, true).use { writer ->
+                        writer.write("\n\n -- PROGRAM CLOSED -- ${Date()}\n//////\n")
+                    }
+                } catch (e: Exception) {
+                    val message: String = if (!exception.isBlank()) "This is probably because there was an error creating the logfile." else ""
+                    if (printErrorMessage && printOutput) println("There was en error writing output into the log file. $message")
+                }
+            }
+
+            if (printOutput && printErrorMessage) println(exception)
+
+            exitProcess(418)
         }
         if (event.keyCode == NativeKeyEvent.VC_O && event.modifiers and NativeKeyEvent.CTRL_MASK != 0) {
 
@@ -210,13 +269,13 @@ class GlobalKeyListener : NativeKeyListener {
 }
 
 
+// function to init read the config
 fun checkConfig(file: File): String {
     if (!file.isFile && !file.exists()) {
         return "null"
     }
 
     val scanner = Scanner(file)
-//    val list = mutableListOf<String>()
     while (scanner.hasNext()) {
         val line = scanner.nextLine()
         return if (line.contains("yes")) {
@@ -231,20 +290,4 @@ fun checkConfig(file: File): String {
     return "null"
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
